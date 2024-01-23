7f93b14084bd3430f5137cfccf6ca1ca5ca14f12
Asif Rahman <57532856+asifrahman18@users.noreply.github.com>
2024-01-23 14:14:53 +0600

    explore page layout

--- frontend/app/api/getJobs/route.ts
+++ frontend/app/api/getJobs/route.ts
@@ -1,4 +1,12 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://127.0.0.1:8000/api';
+
 export async function getJobs() {
-    const res = await fetch(`http://127.0.0.1:8000/api/job/`);
-    return res.json();
+  try {
+    const response = await axios.get(`${BASE_URL}/job/`);
+    return response.data;
+  } catch (error) {
+    throw error;
   }
+}
